21747ca7f67646a92aa71b28607fd1a481ea8267
Mark Arciaga <mark.arciaga@gmail.com>
2016-10-15 11:47:07 -0700

    Updated missing dependencies, added Webpack dev server configuration, adjusted file path imports.

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,35 @@
 var webpack = require('webpack');
+var path = require('path');
 
 module.exports = {
-	entry: './app.js',
-	output: {
-		path: './app',
-		filename: 'bundle.js'
-	},
-	module: {
-		loaders: [
-			{
-			    test: /\.js?$/,
-			    exclude: /(node_modules|bower_components)/,
-			    loader: 'babel'
-			},
-			{
-				test: /\.scss$/,
-				loader: 'style!css!sass'
-			}
-       	]
- },
-	plugins: [
-		new webpack.HotModuleReplacementPlugin()
-   ]
+    entry: './app.js',
+    output: {
+        path: './app',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel'
+            },
+            {
+                test: /\.scss$/,
+                loader: 'style!css!sass'
+            }
+        ]
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, 'app'),
+        progress: true,
+        hot: true,
+        stats: 'errors-only',
+        inline: true,
+        host: '127.0.0.1',
+        port: 8080
+    }
 };
